8c6ddc5f6714ddc092b71adf5bf79b59081d9b01
Rico <ricodewijs1@gmail.com>
2023-09-24 14:56:19 +0200

    added index, pics, busy with css
    baby lore go woo

--- index.php
+++ index.php
@@ -2,40 +2,163 @@
 <html class="no-js" lang="">
 
 <head>
-  <meta charset="utf-8">
-  <title></title>
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <title>Curio</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <meta property="og:title" content="">
-  <meta property="og:type" content="">
-  <meta property="og:url" content="">
-  <meta property="og:image" content="">
+    <meta property="og:title" content="">
+    <meta property="og:type" content="">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
 
-  <link rel="manifest" href="site.webmanifest">
-  <link rel="apple-touch-icon" href="icon.png">
-  <!-- Place favicon.ico in the root directory -->
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="apple-touch-icon" href="icon.png">
+    <!-- Place favicon.ico in the root directory -->
 
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
 
-  <meta name="theme-color" content="#fafafa">
+    <meta name="theme-color" content="#fafafa">
 </head>
 
 <body>
 
-  <!-- Add your site or application content here -->
-  <p>Hello world! This is HTML5 Boilerplate.</p>
-  <script src="js/vendor/modernizr-3.11.2.min.js"></script>
-  <script src="js/plugins.js"></script>
-  <script src="js/main.js"></script>
+    <!-- Add your site or application content here -->
+    <div class="header">
+        <header>
+          <h1>
+            Curio Opendag
+           </h1>
+        </header>
+    </div>
+    <div class="main">
+    </div>
+    <div class="vakken">
+        <p>In het eerste jaar leer je vooral de basis van programmeer- en opmaaktalen. In het tweede jaar werk je
+            met frameworks die bedrijven gebruiken, zoals Laravel, Vue, React, Xamarin en Bootstrap.</p>
+        <p>Codelab is een vak waar je onder begeleiding huiswerk kan maken met begeleiding. Dit is een soort
+            huiswerkbegeleiding.</p>
+        <p>Por: Dit vak omvat regelmatige korte gesprekjes, meestal om de paar weken, waarin studenten kunnen
+            bespreken hoe het op school gaat, hun academische prestaties en eventuele persoonlijke uitdagingen.</p>
+        <p>Mentor uur : Mentoruur biedt studenten persoonlijke begeleiding en advisering door een mentor of docent.
+            Hier worden academische en persoonlijke ontwikkeling besproken en aangepakt.</p>
+        <p>Ned (Nederlands): Ned omvat de studie van de Nederlandse taal, literatuur en communicatieve vaardigheden,
+            waaronder lezen, schrijven en spreken in het Nederlands.</p>
+        <p>Rek (Rekenen): Rek is gericht op wiskundige concepten en vaardigheden, zoals optellen, aftrekken,
+            vermenigvuldigen, delen en problemen oplossen.</p>
+        <p>Eng (Engels): Eng betreft de studie van de Engelse taal, inclusief taalvaardigheid, grammatica en
+            literatuur.</p>
+        <p>Pra (Praktijk): Pra-vakken omvatten praktische oefeningen en ervaringen gerelateerd aan het vakgebied en
+            kunnen variëren afhankelijk van het studieprogramma.</p>
+        <p>Pro: Pro-vakken richten zich op bredere vaardigheden, zoals teamwork, brainstormen, mindmaps en
+            wireframes, naast coderen. Studenten werken aan projecten om deze competenties te ontwikkelen.</p>
+        <p>Native: Native-vakken omvatten het coderen van applicaties en apps in de moedertaal van de student,
+            waardoor ze toepassingen kunnen ontwikkelen die aansluiten bij hun taal en culturele context.</p>
+        <p>Web: Web betreft het coderen van websites, zowel voor persoonlijk gebruik als voor anderen, en omvat het
+            leren van technische aspecten van web en het maken van functionele en esthetische websites.</p>
+    </div>
 
-  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
-  <script>
-    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
-    ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
-  </script>
-  <script src="https://www.google-analytics.com/analytics.js" async></script>
+    <div class="rooster">
+
+        <h2>
+            Dit is het rooster die aan de eerste jaars gegeven word.
+        </h2> 
+        <img src="img/Rooster eerste leerjaar.png" alt="Rooster eerste leerjaar image">
+    </div>
+    <div class="blokindeling">
+        <h2>
+            En dit is de blokindeling van de complete opleiding
+        </h2>
+        <img src="img/Opleidingsoverzicht3.png" alt="overzicht van de blokken">
+    </div>
+
+    <div class="babyLore">
+      <p>dit is een Baby Lore Easteregg, inside jokes zijn geinig</p>
+    </div>
+
+    <div class="docenten">
+        <div class="Wicher teacher">
+            <img src="img/babyteacher.jpg" alt="teacher baby uwu ahaego">
+            <h3>Wicher Hulzebosch</h3>
+            <p>Native - Pro docent</p>
+        </div>
+        <div class="Ine teacher">
+            <img src="img/babyteacher.jpg" alt="teacher baby uwu ahaego">
+            <h3>Ine Vermeer</h3>
+            <p>Web - Mentor docent</p>
+        </div>
+        <div class="Michel teacher">
+            <img src="img/babyteacher.jpg" alt="teacher baby uwu ahaego">
+            <h3>Michel Meeuwesen</h3>
+            <p>Reken docent</p>
+        </div>
+        <div class="Joep teacher">
+            <img src="img/babyteacher.jpg" alt="teacher baby uwu ahaego">
+            <h3>Joep van Gils</h3>
+            <p> Nederlands docent</p>
+        </div>
+        <div class="Mustafa teacher">
+            <img src="img/babyteacher.jpg" alt="teacher baby uwu ahaego">
+            <h3>Mustafa Kasar</h3>
+            <p>Engels docent</p>
+        </div>
+        <div class="Marc teacher">
+            <img src="img/babyteacher.jpg" alt="teacher baby uwu ahaego">
+            <h3>Marc Ridders</h3>
+            <p>Pra docent</p>
+        </div>
+        <div class="Krist teacher">
+            <img src="img/babyteacher.jpg" alt="teacher baby uwu ahaego">
+            <h3>Krist den Dekker</h3>
+            <p>Native docent</p>
+        </div>
+<div class="stage">
+<h2>Mogelijkheid om te versnellen</h2>
+<p>
+Als je het eerste blok lang constant inzet toont met een goede werkhouding, en alle huiswerk opdrachten inlevert met vaak goede resultaten kun je naar het versnelde traject gezet worden die vanaf blok B in gaat. In het versnelde traject kun je de opleiding in 3 jaar in plaats van 4 jaar halen. Hier sla je een paar blokken over en werk je vlotter aan de opdrachten om wel alle benodigde informatie voor de opleiding te begrijpen. In dit traject volg je ook een maar een half jaar stage in plaats van 1,5 jaar. Ten slotte is er ook nog een extra vak, delta, waar je de stof die je normaal bij blok E hoort te krijgen.
+</p>
+</div>
+    </div>
+
+    <footer>
+    <p>&copy Rico de Wijs, student software developer bij curio</p>
+    </footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="js/vendor/modernizr-3.11.2.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
+    <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+    <script>
+    window.ga = function() {
+        ga.q.push(arguments)
+    };
+    ga.q = [];
+    ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto');
+    ga('set', 'anonymizeIp', true);
+    ga('set', 'transport', 'beacon');
+    ga('send', 'pageview')
+    </script>
+    <script src="https://www.google-analytics.com/analytics.js" async></script>
 </body>
 
 </html>
